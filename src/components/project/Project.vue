9f45a88508175949a9dd8a33cd9af2e0d57ec9c2
Jan Reichherzer <jan.reichherzer@gmx.de>
2023-11-01 21:21:35 +0100

    fixed route error in projects comp, deleted old image

--- src/components/project/Project.vue
+++ src/components/project/Project.vue
@@ -1,51 +1,55 @@
 <template>
-	<section class="py-12">
-		<div class="w-full mb-12">
-			<img
-				:src="currentProject.image"
-				:alt="currentProject.altText"
-				class="w-full"
-				loading="lazy"
-			/>
-		</div>
-
-		<div class="flex flex-col gap-16">
-			<div>
-				<h2 class="font-bold text-4xl mb-10">
-					{{ currentProject.title }}
-				</h2>
-				<a :href="currentProject.link" target="_blank" class="text-xl">
-					{{
-						currentProject.linkType === "web" ? "Live preview" : "Behance"
-					}}
-					<i class="fa-solid fa-arrow-right-long"></i>
-				</a>
+	<div>
+		<section class="py-12">
+			<div class="w-full mb-12">
+				<img
+					:src="currentProject.image"
+					:alt="currentProject.altText"
+					class="w-full"
+					loading="lazy"
+				/>
 			</div>
 
-			<div>
-				<h3 class="text-2xl font-bold mb-4">Description</h3>
-				<p class="text-base md:text-lg">
-					{{ currentProject.description }}
-				</p>
-			</div>
+			<div class="flex flex-col gap-16">
+				<div>
+					<h2 class="font-bold text-4xl mb-10">
+						{{ currentProject.title }}
+					</h2>
+					<a :href="currentProject.link" target="_blank" class="text-xl">
+						{{
+							currentProject.linkType === "web"
+								? "Live preview"
+								: "Behance"
+						}}
+						<i class="fa-solid fa-arrow-right-long"></i>
+					</a>
+				</div>
 
-			<div>
-				<h3 class="text-2xl font-bold mb-4">Project goals</h3>
-				<p class="text-base md:text-lg">
-					{{ currentProject.goals }}
-				</p>
-			</div>
+				<div>
+					<h3 class="text-2xl font-bold mb-4">Description</h3>
+					<p class="text-base md:text-lg">
+						{{ currentProject.description }}
+					</p>
+				</div>
 
-			<div>
-				<h3 class="text-2xl font-bold mb-4">Technical info</h3>
-				<ul class="list-disc ml-4">
-					<li v-for="t in currentProject.techInfo">{{ t }}</li>
-				</ul>
-			</div>
-		</div>
-	</section>
+				<div>
+					<h3 class="text-2xl font-bold mb-4">Project goals</h3>
+					<p class="text-base md:text-lg">
+						{{ currentProject.goals }}
+					</p>
+				</div>
 
-	<cta></cta>
+				<div>
+					<h3 class="text-2xl font-bold mb-4">Technical info</h3>
+					<ul class="list-disc ml-4">
+						<li v-for="t in currentProject.techInfo">{{ t }}</li>
+					</ul>
+				</div>
+			</div>
+		</section>
+
+		<cta></cta>
+	</div>
 </template>
 
 <script setup>
